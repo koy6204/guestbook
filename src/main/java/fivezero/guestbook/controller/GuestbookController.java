4e90b4c2a9c050ef118b4edc43ee2e90b8b095da
koy6204 <koy6204@naver.com>
2024-04-02 16:11:29 +0900

    수정

--- src/main/java/fivezero/guestbook/controller/GuestbookController.java
+++ src/main/java/fivezero/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package fivezero.guestbook.controller;
 
+import fivezero.guestbook.dto.GuestbookDTO;
 import fivezero.guestbook.dto.PageRequestDTO;
 import fivezero.guestbook.service.GusetbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -29,5 +32,23 @@ public class GuestbookController {
         log.info("list........." + pageRequestDTO);
 
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+
+        log.info("dto..." + dto);
+
+        //새로추가된 엔티티
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
