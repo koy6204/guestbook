bfd03370cb7e2f4bf9b30d4ff04b54556f93de06
koy6204 <koy6204@naver.com>
2024-04-03 14:35:13 +0900

    게시글 수정,삭제

--- src/main/java/fivezero/guestbook/controller/GuestbookController.java
+++ src/main/java/fivezero/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package fivezero.guestbook.controller;
 
+import fivezero.guestbook.dto.GuestbookDTO;
 import fivezero.guestbook.dto.PageRequestDTO;
 import fivezero.guestbook.service.GusetbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -30,4 +34,44 @@ public class GuestbookController {
 
         model.addAttribute("result", service.getList(pageRequestDTO));
     }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+
+        log.info("gno: " + gno);
+
+        GuestbookDTO dto = service.read(gno);
+
+        model.addAttribute("dto", dto);
+
+    }
+
+    @PostMapping("/remove")
+    public String remove(long gno, RedirectAttributes redirectAttributes) {
+
+        log.info("gno: " + gno);
+
+        service.remove(gno);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
+
+    @PostMapping("/modify")
+    public String modify(GuestbookDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
+
+        log.info("post modify.......");
+        log.info("dto: " + dto);
+
+        service.modify(dto);
+
+        redirectAttributes.addAttribute("Page", requestDTO.getPage());
+        redirectAttributes.addAttribute("gno", dto.getGno());
+
+        return "redirect:/guestbook/read";
+
+
+    }
+
 }
